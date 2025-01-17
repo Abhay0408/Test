3b3709ce100c5cd13fa1a4c122f6502b87a66d5a
Abhay0408 <abhaygp727@gmail.com>
2025-01-17 10:58:21 +0530

    total value

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         int a=5;
-        System.out.println(a);
-
+        int total=0;
+        for(int j = 1; j <= 5; j++) {
+            total += a;
+        }
+        System.out.println(total);
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
